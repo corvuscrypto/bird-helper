b78f93d3f84e34fdbc493e4dd5990ababdc62925
Clifford Richardson <clifford.richardson.@fyndiq.com>
2018-04-06 14:00:08 +0200

    Added threading and proper thread-safe buffering

--- bird_helper.py
+++ bird_helper.py
@@ -1,6 +1,48 @@
+import time
 import requests
+import threading
+from queue import Queue
+
+MIN_BUFFER_LEN = 4
 
 FIFO_FILE = None
+
+QUEUE = Queue(10)
+
+
+class DataThread(threading.Thread):
+    def __init__(self, queue):
+        super(DataThread, self).__init__()
+        self.queue = queue
+        self.last_four = []
+
+    def run(self):
+        while True:
+            for url in self.get_urls():
+                self.get_new_data(url, self.last_four)
+            time.sleep(1)
+
+    def buffer_data(self, url):
+        body = requests.get(url).content
+        self.queue.put(body)
+
+    def get_new_data(self, url, last_four):
+        if url in last_four:
+            return
+        if len(last_four) == 4:
+            last_four.pop(0)
+        last_four.append(url)
+        self.buffer_data(url)
+
+    def get_urls(self):
+        resp = requests.get("http://tv.eenet.ee/hls/merikotkas.m3u8")
+        text = resp.text
+        urls = []
+        for line in text.split("\n"):
+            if "#" in line:
+                continue
+            urls.append("http://tv.eenet.ee/hls/"+line)
+        return urls
 
 
 def open_fifo(name):
@@ -8,40 +50,24 @@ def open_fifo(name):
     FIFO_FILE = open(name, "wb")
 
 
-LAST_FOUR = []
-
-
-def pipe_data(url):
-    body = requests.get(url).content
-    FIFO_FILE.write(body)
-
-
-def get_new_data(url):
-    if url in LAST_FOUR:
-        return
-    if len(LAST_FOUR) == 4:
-        LAST_FOUR.pop(0)
-    LAST_FOUR.append(url)
-    pipe_data(url)
-
-
-def get_urls():
-    resp = requests.get("http://tv.eenet.ee/hls/merikotkas.m3u8")
-    text = resp.text
-    urls = []
-    for line in text.split("\n"):
-        if "#" in line:
-            continue
-        urls.append("http://tv.eenet.ee/hls/"+line)
-    return urls
+def write_buffer():
+    data = QUEUE.get()
+    FIFO_FILE.write(data)
+    QUEUE.task_done()
 
 
 def main(filename):
     open_fifo(filename)
-    while True:
-        for url in get_urls():
-            get_new_data(url)
+
+    th = DataThread(QUEUE)
+    th.start()
+
+    while QUEUE.unfinished_tasks < MIN_BUFFER_LEN:
         time.sleep(1)
+
+    while True:
+        write_buffer()
+
 
 if __name__ == "__main__":
     import sys
